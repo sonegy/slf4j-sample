8ce5242a6afdafb5f225025d0bd7c58e0062ffb5
Jae-il Lee <sonegy@sk.com>
2014-05-22 18:27:12 +0900

    jcl-over-slf4j 추가

--- src/main/java/sonegy/slf4j/sample/Sample.java
+++ src/main/java/sonegy/slf4j/sample/Sample.java
@@ -2,19 +2,26 @@ package sonegy.slf4j.sample;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.StaticApplicationContext;
 
 /**
  * @author: sonegy@sk.com
  */
 public class Sample {
-    final Logger logger = LoggerFactory.getLogger(Sample.class);
+    private static final Logger logger = LoggerFactory.getLogger(Sample.class);
 
-    public void run() {
+    public static void run() {
+        // logback
         logger.debug("debug");
         logger.info("info");
+
+        // common logging
+        StaticApplicationContext context = new StaticApplicationContext();
+        context.registerSingleton("test", Sample.class);
+        context.getBean("test", Sample.class);
     }
 
     public static void main(String[] args) {
-        new Sample().run();
+        run();
     }
 }
